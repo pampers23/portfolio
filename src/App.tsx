e6d6375cdd57eaeb421f2437ad72085083b0adae
Lance Brixter Mendoza <lance.vizserve@gmail.com>
2025-05-23 09:05:24 +0800

    creating homepage/Navbar

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Home } from "./components/Home/Home"
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-white dark:bg-neutral-950">
+      <Home />
+    </div>
+  )
+}
+
+export default App
