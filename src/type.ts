126cb0c8912ad574568c5da710858a28468c66b7
pampers23 <dexterpimss23@gmail.com>
2026-04-27 17:24:41 +0800

    feat: add projects section

--- src/type.ts
+++ src/type.ts
@@ -7,3 +7,16 @@ export type ExperienceItem = {
   side: "left" | "right";
   index: number;
 }
+
+export type Project = {
+  slug: string;
+  title: string;
+  tagline: string;
+  description: string;
+  image: string;
+  stack: string[];
+  features: string[];
+  technologies: string[];
+  liveUrl?: string;
+  repoUrl?: string;
+}
