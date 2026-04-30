d050dbaf792170781489004fa4eddd1d6b1957fb
pampers23 <dexterpimss23@gmail.com>
2026-04-30 11:46:53 +0800

    feat: add loader

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,35 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from '@tanstack/react-router'
 import { getRouter } from './router'
 import { Toaster } from 'sonner'
+import Loader from './components/loader'
 
 const router = getRouter()
 
+const Root = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 10000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) return <Loader />
+
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster position="bottom-right" richColors duration={3000} className="sm:!top-auto sm:!bottom-4 sm:!right-4 sm:!left-auto" />
+    </>
+  )
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <Toaster position="bottom-right" richColors duration={3000} className="sm:!top-auto sm:!bottom-4 sm:!right-4 sm:!left-auto" />
+    <Root />
   </StrictMode>,
 )
